eeea55daaa452b8fc02f7b4d5000b0f10ad4902d
n.severinchik <na.severinchik@gmail.com>
2022-01-16 16:43:02 +0300

    Initial commit

---
+++ app/src/main/java/na/severinchik/pp2_2022/SecondActivity.kt
@@ -0,0 +1,11 @@
+package na.severinchik.pp2_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
